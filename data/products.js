f726fb11a099c911bd6c45fd9c11719cb8c018c1
RN <r.nanwani@gmail.com>
2021-07-13 14:13:06 +0000

    First commit

---
+++ data/products.js
@@ -0,0 +1,151 @@
+module.exports = [{
+    id: 1,
+    name: 'Rolex Watch',
+    type: 'Watches',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-watch-1.jpg',
+      small: '/images/content/small/product-watch-1.jpg',
+      large: '/images/content/large/product-watch-1.jpg'
+    }
+  }, {
+    id: 2,
+    name: 'Colorful Dress',
+    type: 'Dresses',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-clothes-6.jpg',
+      small: '/images/content/small/product-clothes-6.jpg',
+      large: '/images/content/large/product-clothes-6.jpg'
+    }
+  }, {
+    id: 3,
+    name: 'Open Book',
+    type: 'Books',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-book-2.jpg',
+      small: '/images/content/small/product-book-2.jpg',
+      large: '/images/content/large/product-book-2.jpg'
+    }
+  }, {
+    id: 4,
+    name: 'Blue Bike',
+    type: 'Bikes',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-bike-2.jpg',
+      small: '/images/content/small/product-bike-2.jpg',
+      large: '/images/content/large/product-bike-2.jpg'
+    }
+  }, {
+    id: 5,
+    name: 'White Dress',
+    type: 'Dresses',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-clothes-5.jpg',
+      small: '/images/content/small/product-clothes-5.jpg',
+      large: '/images/content/large/product-clothes-5.jpg'
+    }
+  }, {
+    id: 6,
+    name: 'Rider Watch',
+    type: 'Watches',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-watch-2.jpg',
+      small: '/images/content/small/product-watch-2.jpg',
+      large: '/images/content/large/product-watch-2.jpg'
+    }
+  }, {
+    id: 7,
+    name: 'Yellow Book',
+    type: 'Books',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-book-1.jpg',
+      small: '/images/content/small/product-book-1.jpg',
+      large: '/images/content/large/product-book-1.jpg'
+    }
+  }, {
+    id: 8,
+    name: 'Blue Tshirt',
+    type: 'Clothes',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-clothes-3.jpg',
+      small: '/images/content/small/product-clothes-3.jpg',
+      large: '/images/content/large/product-clothes-3.jpg'
+    }
+  }, {
+    id: 9,
+    name: 'Blue Jeans',
+    type: 'Clothes',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-clothes-1.jpg',
+      small: '/images/content/small/product-clothes-1.jpg',
+      large: '/images/content/large/product-clothes-1.jpg'
+    }
+  }, {
+    id: 10,
+    name: 'Black Bike',
+    type: 'Bikes',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-bike-1.jpg',
+      small: '/images/content/small/product-bike-1.jpg',
+      large: '/images/content/large/product-bike-1.jpg'
+    }
+  }, {
+    id: 11,
+    name: 'Citizen Watch',
+    type: 'Watches',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-watch-3.jpg',
+      small: '/images/content/small/product-watch-3.jpg',
+      large: '/images/content/large/product-watch-3.jpg'
+    }
+  }, {
+    id: 12,
+    name: 'Blue Suit',
+    type: 'Clothes',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-clothes-2.jpg',
+      small: '/images/content/small/product-clothes-2.jpg',
+      large: '/images/content/large/product-clothes-2.jpg'
+    }
+  }, {
+    id: 13,
+    name: 'White T-shirt',
+    type: 'Clothes',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-clothes-4.jpg',
+      small: '/images/content/small/product-clothes-4.jpg',
+      large: '/images/content/large/product-clothes-4.jpg'
+    }
+  }, {
+    id: 14,
+    name: 'iPhone',
+    type: 'Phones',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/product-mobile-1.jpg',
+      small: '/images/content/small/product-mobile-1.jpg',
+      large: '/images/content/large/product-mobile-1.jpg'
+    }
+  }, {
+    id: 15,
+    name: 'Landscape Location',
+    type: 'Photographs',
+    href: 'product-detail',
+    image: {
+      original : 'https://ik.imagekit.io/hqimagedemo/hqimage_demo/original/landscape-1.jpg',
+      small: '/images/content/small/landscape-1.jpg',
+      large: '/images/content/large/landscape-1.jpg'
+    }
+  }]
